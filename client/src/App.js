ce1c26d95a19b9eccf520712f135964a1acbe5f3
Anh Vo <votqanh@gmail.com>
2023-11-19 14:32:45 -0800

    added discord button import

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import UserProfile from './components/UserProfile';
 import RecommendedProfile from './components/RecommendedProfile';
 import Header from './components/Header'
+import DiscordLoginButton from './components/DiscordLoginButton';
 
 const App = () => {
   return (
