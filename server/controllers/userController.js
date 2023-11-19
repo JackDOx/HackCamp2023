88788f9d045372d44f041b6bc0af06b4facf6311
Trung Nguyen <atrung380@gmail.com>
2023-11-19 13:58:31 -0800

    test json

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -28,3 +28,58 @@ exports.getAllUsers = catchAsync( async (req, res, next) => {
     users: doc
   });
 });
+
+exports.updateProfile = catchAsync(async (req, res, next) => {
+  // Assuming req.body contains a JSON object with user data
+  const user = req.body;
+
+  const usernameToUpdate = user.discordName;
+
+  if (!user.profilePic) {
+    console.log('No profile picture found');
+  }
+
+  // Decode base64 data and create a buffer
+  const imageBuffer = Buffer.from(user.profilePic, 'base64');
+  const filename = `${usernameToUpdate}.png`;
+
+  // Resize and save the image to local storage
+  sharp(imageBuffer)
+    .resize(500, 500)
+    .toFormat('png')
+    .jpeg({ quality: 90 })
+    .toFile(`devdata/img/users/${filename}`, (err) => {
+      if (err) {
+        return next(err); // Pass the error to the error handling middleware
+      }
+
+      console.log('Profile picture saved successfully:', filename);
+
+      // Update the user in MongoDB
+      User.updateOne({ username: usernameToUpdate }, {
+        $set: {
+          name: user.name,
+          role: user.role,
+          discordName: user.discordName,
+          skillLevel: user.skillLevel,
+          userRole: user.userRole,
+          interests: user.interests,
+          pronoun: user.pronoun,
+          lookingForTeam: user.lookingForTeam,
+          photo: `devdata/img/users/${filename}`,
+        },
+      })
+        .then((result) => {
+          console.log('User updated successfully:', result);
+          res.status(201).json({
+            status: 'Success',
+            data: {
+              user: result,
+            },
+          });
+        })
+        .catch((updateError) => {
+          return next(updateError); // Pass the update error to the error handling middleware
+        });
+    });
+});
