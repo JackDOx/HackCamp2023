b7d917c43fdd8e1e172cb7ee6ac427fb41220b1a
Trung Nguyen <atrung380@gmail.com>
2023-11-19 18:46:36 -0800

    update stuffs

--- client/src/components/RecommendedProfile.js
+++ client/src/components/RecommendedProfile.js
@@ -1,36 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './RecommendedProfile.css';
-import { useEffect } from 'react';
-import DefaultProfilePic from '../images/default-profile-pic.jpeg'
-import ProfilePicDisplay from './ProfilePicDisplay';
-import HoverableHeader from './HoverableHeader'
+import axios from 'axios';
 
 function RecommendedProfile() {
-  const name = ""
-  const pronouns = "";
-  const skillLevel = "";
-  const interests = ""; 
-  const preferredRole = "";
-  const lookingForTeammates = "";
+  const [profileData, setProfileData] = useState([]);
+  const [currentCardIndex, setCurrentCardIndex] = useState(0);
+  var index = 0;
 
   useEffect(() => {
-    // const user = req.user.username; // Replace this with the actual way you access req.user on the frontend
-  
-    fetch('http://localhost:3001/api/users/getAllUsers', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
+    fetch('http://localhost:3001/api/users/getAllUsers')
       .then(response => response.json())
       .then(data => {
-        const users = data.users[0];
-        name = users.name;
-        pronouns = users.pronoun;
-        skillLevel = users.skillLevel;
-        interests = users.interests; 
-        preferredRole = users.preferredRole;
-        lookingForTeammates = users.lookingForTeammates;
+        setProfileData(data.users[index]);
       })
       .catch(error => {
         console.error('Error fetching data:', error);
@@ -38,7 +19,7 @@ function RecommendedProfile() {
   }, []);
 
   const handleSwipeLeft = () => {
-    // // Perform actions when swiped left
+    // Perform actions when swiped left
     // console.log('Swiped left on card:', profileData.name);
     // // Move to the next card
     // axios.get('http://localhost:3001/api/users/swipe', {
@@ -53,11 +34,12 @@ function RecommendedProfile() {
     //     console.error('Error swiping left:', error);
     //   });
 
-    setCurrentCardIndex(prevIndex => (prevIndex + 1) % 3); // Assuming 3 cards for example
+    // setCurrentCardIndex(prevIndex => (prevIndex + 1) % 3); // Assuming 3 cards for example
+    index = index + 1;
   };
 
   const handleSwipeRight = () => {
-    // // Perform actions when swiped right
+    // Perform actions when swiped right
     // console.log('Swiped right on card:', profileData.name);
     // // Move to the next card
     // axios.get('http://localhost:3001/api/users/swipe', {
@@ -71,42 +53,45 @@ function RecommendedProfile() {
     //   .catch(error => {
     //     console.error('Error swiping left:', error);
     //   });
-    setCurrentCardIndex(prevIndex => (prevIndex + 1) % 3); // Assuming 3 cards for example
+    index = index + 1;
+    // setCurrentCardIndex(prevIndex => (prevIndex + 1) % 3); // Assuming 3 cards for example
   };
 
   return (
     <div>
-        <HoverableHeader />
-        <div className="user-profile-card recommended-profile-card">
-            <form>
-                {/* Display each piece of information in a similar layout to UserProfile */}
-                <ProfilePicDisplay file={DefaultProfilePic} />
-                
-                <div className="flex-container">
-                <label>Name:</label>
-                <span>{name} ({pronouns})</span>
-                </div>
+      <div className="user-profile-card">
+        <form>
+          {/* Display each piece of information in a similar layout to UserProfile */}
+          <div className="flex-container">
+            <label>Name:</label>
+            <span>{profileData.name}</span>
+          </div>
 
-                <div className="flex-container">
-                <label>Skill Level:</label>
-                <span>{skillLevel}</span>
-                </div>
+          <div className="flex-container">
+            <label>Pronouns:</label>
+            <span>{profileData.pronoun}</span>
+          </div>
 
-                <label>Interests / Project Vision:</label>
-                <p>{interests}</p>
+          <div className="flex-container">
+            <label>Skill Level:</label>
+            <span>{profileData.skillLevel}</span>
+          </div>
 
-                <div className="flex-container">
-                <label>Preferred Role:</label>
-                <span>{preferredRole}</span>
-                </div>
-            </form>
-        </div>
+          <label>Interests / Project Vision:</label>
+          <p>{profileData.interests}</p>
+
+          <div className="flex-container">
+            <label>Preferred Role:</label>
+            <span>{profileData.preferredRole}</span>
+          </div>
+        </form>
+      </div>
       {/* Add heart and X buttons outside the card */}
       <div className="button-container">
-        <button type="button" className="cross-button">
+        <button type="button" className="cross-button" onClick={handleSwipeLeft}>
           <i className="fa fa-times"></i>
         </button>
-        <button type="button" className="heart-button">
+        <button type="button" className="heart-button" onClick={handleSwipeRight}>
           <i className="fa fa-heart"></i>
         </button>
       </div>
